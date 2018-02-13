1d254c30a91a8cf980549b984f38892b282b7c3d
theschitz <alex@theschitz.se>
2018-02-13 17:15:01 +0100

    Update colors.py

--- colors.py
+++ colors.py
@@ -7,3 +7,19 @@ def green():
         
 def blue():
         return(0, 0, 255)
+
+                #chocolate
+        #colorCode(210,105,30)
+        #time.sleep(2)
+
+        #Blue violett
+        #colorCode(138,43,226)
+        #time.sleep(2)
+
+        #dodger blue
+        #colorCode(30,144,255)
+        #time.sleep(2)
+
+        #Cyan/aqua
+        #colorCode(0,255,255)
+        #time.sleep(2)
