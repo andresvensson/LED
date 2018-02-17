2f82cb713786597f46591d40d0e93d5fec9728da
andresvensson <34170640+andresvensson@users.noreply.github.com>
2018-02-17 18:41:24 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+def set():
+        r, g, b = colors.Yellow()
+        led.colorCode(r, g, b) 
+
+set()
+
+
+# Problem
+# This so work but I want to something that makes me change color with the line: 
+# set(Yellow)
+
+
